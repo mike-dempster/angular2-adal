fc867a5e1e833bebdd7dc601f2ac4709ec22e477
Mike Dempster <michael.j.dempster@gmail.com>
2017-10-26 19:05:15 -0700

    Updated for new NPM package
    Removed dist from source control and updated the package config for new npm package

--- dist/ts/core.ts
+++
@@ -1,8 +0,0 @@
-/**
- * ng2-adal - Use Azure AD Library - ADAL in Angular 4 & 2. For Angular 4, use latest version 1.x.x. For Angular 2 use 0.3.1 version.
- * @version v2.0.2
- * @link https://github.com/sureshchahal/angular2-adal#readme
- * @license MIT
- */
-export {AdalService, OAuthData, AuthHttp} from './services';
-export { AdalModule, AuthHttpModule } from './modules';
